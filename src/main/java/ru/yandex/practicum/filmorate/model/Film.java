6492ae0e135f419c53d2c5f9cc8738f393de3348
kiselkakakaka <klim200387@gmail.com>
2025-08-01 10:36:05 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,28 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Data;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
+@Data
 public class Film {
+    private int id;
+
+    @NotBlank
+    private String name;
+
+    @Size(max = 200)
+    private String description;
+
+    @PastOrPresent
+    private LocalDate releaseDate;
+
+    @Positive
+    private int duration;
 }
+
