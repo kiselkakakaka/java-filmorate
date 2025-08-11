c739e084c25a11ee40a9674c404b74f36fc24039
kiselkakakaka <klim200387@gmail.com>
2025-08-11 21:07:29 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,8 +6,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
+
     User add(User user);
+
     User update(User user);
+
     Optional<User> getById(int id);
+
     List<User> getAll();
 }
