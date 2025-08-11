59b863d26d48eaca1ecc0e7da8be523d63b68c87
kiselkakakaka <klim200387@gmail.com>
2025-08-11 21:18:29 +0300

    fix

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -2,11 +2,17 @@ package ru.yandex.practicum.filmorate.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.yandex.practicum.filmorate.exception.GlobalExceptionHandler;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.time.LocalDate;
 
@@ -14,25 +20,29 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(FilmController.class)
-public class FilmControllerTest {
+@Import(GlobalExceptionHandler.class)
+class FilmControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @Autowired MockMvc mockMvc;
+    @Autowired ObjectMapper mapper;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    @MockBean FilmService filmService;
 
     @Test
     void shouldRejectFilmWithTooEarlyReleaseDate() throws Exception {
-        Film film = new Film();
-        film.setName("Очень старый фильм");
-        film.setDescription("Ещё до кино");
-        film.setDuration(100);
-        film.setReleaseDate(LocalDate.of(1800, 1, 1));
+        Film bad = new Film();
+        bad.setName("Old");
+        bad.setDescription("desc");
+        bad.setDuration(100);
+        bad.setReleaseDate(LocalDate.of(1800, 1, 1));
+
+               Mockito.when(filmService.create(Mockito.any()))
+                .thenThrow(new ValidationException("Дата релиза не может быть раньше 28.12.1895"));
 
         mockMvc.perform(post("/films")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(film)))
+                        .content(mapper.writeValueAsString(bad)))
                 .andExpect(status().isBadRequest());
     }
 }
+
