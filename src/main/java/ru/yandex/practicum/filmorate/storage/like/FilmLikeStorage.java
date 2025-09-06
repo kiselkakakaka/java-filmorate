268c7d375d07a020eebeac3caf2416e3808fbfb7
kiselkakakaka <klim200387@gmail.com>
2025-09-06 04:18:02 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/like/FilmLikeStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/like/FilmLikeStorage.java
@@ -5,7 +5,10 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmLikeStorage {
+
     void addLike(int filmId, int userId);
+
     void removeLike(int filmId, int userId);
+
     List<Film> findPopular(int count);
 }
