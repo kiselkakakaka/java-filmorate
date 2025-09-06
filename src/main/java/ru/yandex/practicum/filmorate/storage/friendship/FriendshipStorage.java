a5ad670f36c4e35607617a159886636ab83d3d37
kiselkakakaka <klim200387@gmail.com>
2025-09-06 04:16:26 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/friendship/FriendshipStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/friendship/FriendshipStorage.java
@@ -5,9 +5,12 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface FriendshipStorage {
+
     void addFriend(int userId, int friendId);
+
     void removeFriend(int userId, int friendId);
+
     List<User> getFriends(int userId);
+
     List<User> getCommonFriends(int userId, int otherId);
 }
-
