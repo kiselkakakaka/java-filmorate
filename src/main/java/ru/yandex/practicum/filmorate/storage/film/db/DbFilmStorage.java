c75ea4e233129099e864d0b0381b5b191cef3844
kiselkakakaka <klim200387@gmail.com>
2025-09-06 04:14:37 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/db/DbFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/db/DbFilmStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.film.db;
 
 import org.springframework.context.annotation.Primary;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
